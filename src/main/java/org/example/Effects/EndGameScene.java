996a40c464d83558d1e63f86d23a8beddb6a03d6
JBRKR000 <kozimorjakub@gmail.com>
2025-04-02 19:24:24 +0200

    - fix

--- src/main/java/org/example/Effects/EndGameScene.java
+++ src/main/java/org/example/Effects/EndGameScene.java
@@ -24,23 +24,41 @@ public class EndGameScene extends SubScene {
         ImageView backgroundView = new ImageView(backgroundImage);
         backgroundView.setFitWidth(FXGL.getAppWidth());
         backgroundView.setFitHeight(FXGL.getAppHeight());
+
         Text message = new Text("Gratulacje! Ukończyłeś grę!");
         Text message2 = new Text("Twój wynik: " + finalScore);
+        Text thankYouMessage = new Text("Dziękujemy za przejście wersji demo. Mamy nadzieję, że się spodobało!");
+
         message.setFill(Color.WHITE);
         message2.setFill(Color.WHITE);
+        thankYouMessage.setFill(Color.WHITE);
+
         message.setFont(Font.font("Comic Sans MS", 24));
         message2.setFont(Font.font("Comic Sans MS", 24));
+        thankYouMessage.setFont(Font.font("Comic Sans MS", 24));
+
         message2.setTextAlignment(TextAlignment.CENTER);
+        thankYouMessage.setTextAlignment(TextAlignment.CENTER);
+
         FadeTransition fadeTransition = new FadeTransition(Duration.seconds(2), message);
         FadeTransition fadeTransition2 = new FadeTransition(Duration.seconds(2), message2);
+        FadeTransition fadeTransition3 = new FadeTransition(Duration.seconds(2), thankYouMessage);
+
         fadeTransition.setFromValue(0);
         fadeTransition.setToValue(1);
         fadeTransition.setCycleCount(1);
         fadeTransition.play();
+
         fadeTransition2.setFromValue(0);
         fadeTransition2.setToValue(1);
         fadeTransition2.setCycleCount(1);
         fadeTransition2.play();
+
+        fadeTransition3.setFromValue(0);
+        fadeTransition3.setToValue(1);
+        fadeTransition3.setCycleCount(1);
+        fadeTransition3.play();
+
         Button mainMenuButton = new Button("Powrót do menu głównego");
         mainMenuButton.setStyle("-fx-background-color: #ff0000; -fx-text-fill: white; -fx-font-size: 16px;");
         mainMenuButton.setOnAction(e -> {
@@ -50,13 +68,14 @@ public class EndGameScene extends SubScene {
             wave = 1;
             FXGL.getAudioPlayer().playMusic(InitSettings.currentMusic);
         });
-        VBox layout = new VBox(10, message, message2, mainMenuButton);
+
+        VBox layout = new VBox(10, message, message2, thankYouMessage, mainMenuButton);
         layout.setAlignment(Pos.CENTER);
         layout.setTranslateX((double) FXGL.getAppWidth() / 2 - 1000);
         layout.setTranslateY((double) FXGL.getAppHeight() / 2 - 650);
+
         StackPane root = new StackPane(backgroundView, layout);
         root.setAlignment(Pos.CENTER);
         getContentRoot().getChildren().add(root);
-
     }
 }
